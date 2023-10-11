6b19281a1f6412443864dedb7a33a4a38a94694e
Henry Reyes <reyesh2@uci.edu>
2023-10-10 19:52:02 -0700

    session provider

---
+++ components/Providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { SessionProvider } from 'next-auth/react';
+import React, { Children, ReactNode } from 'react'
+
+interface Props{
+    children: ReactNode;
+}
+
+export default function Providers({ children }: Props) {
+  return (
+    <SessionProvider>{children}</SessionProvider>
+  )
+}
