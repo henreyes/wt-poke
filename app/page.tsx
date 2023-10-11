349554213a0b0f68a7670942d514c6d3fce2ca45
Henry Reyes <reyesh2@uci.edu>
2023-10-11 13:36:14 -0700

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-28 bg-zinc-900">
      
-      <div className=" flex flex-row text-cente group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 ">
+      <div className=" flex flex-row text-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 ">
         
         <SignInButton />
 
