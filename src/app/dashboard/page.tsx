9d2f1ecc6d38eae2c804c5c2ed9df5544d1f4f3c
Henry Reyes <reyesh2@uci.edu>
2023-10-16 14:57:08 -0700

    dashboard layout for user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,38 +1,67 @@
 
+"use client";
 import { getServerSession } from "next-auth";
 import { authConfig } from "../api/auth/[...nextauth]/route";
 import Logout from "../logout";
 import Image from 'next/image'
 import GameCard from "../gameCard/gc";
+import Card from "../card/card";
+
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import Modal from "../modal/modal";
 
 
-export default async function Dashboard () {
-    const session = await getServerSession(authConfig);
+export default  function Dashboard () {
+    const session =  useSession();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  
+    const handleOpenModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleCloseModal = () => {
+      setIsModalOpen(false);
+    };
+  
+    const handleOptionSelected = (option: string) => {
+      setSelectedOption(option);
+      setIsModalOpen(false); // Close the modal
+    };
+  
+   
    
     return (
         <>
             <div className=" flex flex-row justify-between text-black bg-red-100 p-2   ">
-                <p className="text-md font-bold px-2 ">Hello, {session?.user.username}</p>
-                <p className="text-md font-light px-2 "> pokemon found: {session?.user.currentScore} </p>
+                <p className="text-md font-bold px-2 ">Hello, { session.data?.user.username}</p>
+                <p className="text-md font-light px-2 "> pokemon found: { session.data?.user.pokeFound} </p>
                 <Logout />
             </div>
-            
+
+            <div>
+            <button onClick={handleOpenModal} className="bg-gradient-to-r from-blue-400 to-blue-900 text-white active:from-purple-600 active:via-pink-700 active:to-red-600 font-bold uppercase text-sm px-6 py-3 rounded-full shadow-md hover:shadow-lg transition duration-300 ease-in-out">
+            Choose Gen
+      </button>
+
+      <p>Selected Option: {selectedOption}</p>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onOptionSelected={handleOptionSelected}
+        options={['Gen I', 'Gen II ', 'Gen III', 'Gen IV', 'Gen V']}
+        title="Choose Gen"
+      />
+    </div>
+           
             <div className=" flex flex-col h-screen items-center justify-center px-20 py-10 bg-slate-600 ">
+
                 <div className="flex flex-col items-center bg-slate-800 rounded-xl w-full h-full">
-                    <div className="">
-                        <details className="dropdown text-white">
-                            <summary className="p-1 btn flex-col font-bold">Dificulty</summary>
-                            <ul className="p-3 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 rounded-lg text-white font-light text-sm bg-white bg-opacity-50 ">
-                                <li className="rounded-md text-center transition ease-in-out delay-10 bg-blue-300 hover:-translate-y-1 hover:scale-110"><a>Gen 1 </a></li>
-                                <li className="rounded-md text-center transition ease-in-out delay-10 bg-blue-500 hover:-translate-y-1 hover:scale-110"><a>Gen 2</a></li>
-                                <li className="rounded-md text-center transition ease-in-out delay-10 bg-blue-700 hover:-translate-y-1 hover:scale-110"><a>Gen 3</a></li>
-                                <li className="rounded-md text-center transition ease-in-out delay-10 bg-blue-800 hover:-translate-y-1 hover:scale-110"><a>Gen 4</a></li>
-                                <li className="rounded-md text-center transition ease-in-out delay-10 bg-blue-950 hover:-translate-y-1 hover:scale-110"><a>Gen 5</a></li>
-                            </ul>
-                        </details>
-                    </div>        
-                        
-                    <GameCard/>
+                    
+             
+                    <Card imgSrc="/bulb.jpeg" />
                     
                 </div>
 
@@ -45,3 +74,4 @@ export default async function Dashboard () {
 </>
 )
 }
+
