2567df4e182dd9ccda2584830be3944da044a175
Henry Reyes <reyesh2@uci.edu>
2023-10-17 19:29:12 -0700

    new route redirect

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from 'next/navigation';
 export default async function LoginPage() {
   const session = await getServerSession();
   if (session) {
-    redirect('/dashboard');
+    redirect(`/user/${session.user.username}`);
   }
   return <Form />;
 }
