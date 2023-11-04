6b89d74967bc41bab493b74299c48c1f813dd308
Henry Reyes <reyesh2@uci.edu>
2023-11-04 12:05:24 -0700

    style changes

--- src/app/user/[username]/[id]/page.tsx
+++ src/app/user/[username]/[id]/page.tsx
@@ -5,13 +5,16 @@ import { getStats } from "./_actions";
 export default async function GameHome({ params }: { params: { username: string, id: number } }){
   const userStats = await getStats(params.username);
   console.log(userStats);
+  if (!userStats){
+    return null;
+  }
 
 
   return (
     <>
       <div>{userStats?.username}</div>
       <div>Poke found: {userStats?.pokeFound}</div>
-      <GameComponent params={{ username: params.username,  id: Number(params.id) }} />
+      <GameComponent params={{ username: params.username,  id: Number(params.id), pokeFound: userStats?.pokeFound }} />
     </>
   )
 }
