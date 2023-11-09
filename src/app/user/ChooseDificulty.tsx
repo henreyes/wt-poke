9e239f445f565471195a5779bf98a7ba6a86f3fc
Henry Reyes <reyesh2@uci.edu>
2023-11-08 22:13:57 -0800

    fetching initial data

--- src/app/user/ChooseDificulty.tsx
+++ src/app/user/ChooseDificulty.tsx
@@ -1,47 +1,57 @@
-// components/ChooseDifficulty.js
+// components/ChooseDifficulty.tsx
 "use client"
-// components/ChooseDifficulty.js
 import { useState } from 'react';
-type Difficulty = 'Gen 1' | 'Gen 2' | 'Gen 3' ;
+import { useRouter } from 'next/navigation';
 
-const typeToColor: { [key in Difficulty]?: string } = {
+type Difficulty = 'Gen 1' | 'Gen 2' | 'Gen 3';
+
+const typeToColor: { [key in Difficulty]: string } = {
   "Gen 1": 'bg-green-500',
   "Gen 2": 'bg-purple-500',
   "Gen 3": 'bg-red-500',
 };
 
-const activeColor: { [key in Difficulty]?: string } = {
-  "Gen 1": 'bg-green-800',
-  "Gen 2": 'bg-purple-800',
-  "Gen 3": 'bg-red-800',
-};
+interface ChooseDifficultyProps {
+  username: string;
+}
 
-export default function ChooseDifficulty({ onSelectDifficulty }: { onSelectDifficulty: (difficulty: Difficulty) => void }) {
-  const difficulties: Difficulty[] = ['Gen 1', 'Gen 2', 'Gen 3'];
+const ChooseDifficulty: React.FC<ChooseDifficultyProps> = ({ username }) => {
   const [selectedDifficulty, setSelectedDifficulty] = useState<Difficulty | null>(null);
+  const router = useRouter();
 
-  const handleSelectDifficulty = (gen: Difficulty) => {
-    setSelectedDifficulty(gen);
-    onSelectDifficulty(gen);
+  const getRandomNumber = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const playGame = () => {
+    if (selectedDifficulty) {
+      const randomNumber = getRandomNumber(1, 100);
+      router.push(`/user/${username}/${randomNumber}`);
+    }
   };
 
   return (
-    <div className="flex space-x-4 mb-40 mt-10">
-      {difficulties.map((gen, index) => (
-        <button
-          key={index}
-          className={`relative w-24 h-24 ${selectedDifficulty === gen ? activeColor[gen] : typeToColor[gen] || 'bg-slate-200'} rounded-full cursor-pointer flex items-center justify-center transform transition-all duration-300 ${selectedDifficulty === gen ? 'w-32 h-32' : ''}`}
-          onClick={() => handleSelectDifficulty(gen)}
-          aria-label={`Select ${gen}`}
-        >
-          <span className="text-white text-lg font-bold">{gen}</span>
-          {selectedDifficulty === gen && (
-            <div className="absolute top-full mt-2 w-32 bg-white p-2 shadow rounded transition-opacity duration-300 ease-in-out text-gray-700">
-              <p>Information about {gen}</p>
-            </div>
-          )}
-        </button>
-      ))}
-    </div>
+    <>
+      <div className="flex space-x-4 mb-10">
+        {(['Gen 1', 'Gen 2', 'Gen 3'] as Difficulty[]).map((gen) => (
+          <button
+            key={gen}
+            className={`w-24 h-24 ${typeToColor[gen]} rounded-full cursor-pointer flex items-center justify-center ${selectedDifficulty === gen ? 'scale-125' : ''}`}
+            onClick={() => setSelectedDifficulty(gen)}
+          >
+            <span className="text-white text-lg font-bold">{gen}</span>
+          </button>
+        ))}
+      </div>
+      <button
+        disabled={!selectedDifficulty}
+        className={`text-xl px-6 py-3 rounded-full transition duration-300 ease-in-out transform ${selectedDifficulty ? 'bg-blue-500 hover:bg-blue-600 text-white' : 'bg-blue-300 text-gray-500 cursor-not-allowed'}`}
+        onClick={playGame}
+      >
+        {selectedDifficulty ? 'Play Game' : 'Select Difficulty'}
+      </button>
+    </>
   );
-}
+};
+
+export default ChooseDifficulty;
--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,47 +1,69 @@
-"use client"
+
 import Link from "next/link";
 import Logout from "../../logout";
 import { prisma } from "@/lib/prisma";
 import { AllPokemon } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import ChooseDifficulty from "../ChooseDificulty";
-import router, { useRouter } from "next/navigation";
 import { useState } from "react";
 
-let pokemon: AllPokemon = {
-    id: 0,
-    name: "template name",
-    frontDefault: "",
-    officialArtwork: "",
-    url: "",
-};
-type Difficulty = 'Gen 1' | 'Gen 2' | 'Gen 3' | null;
+const typeToColor: { [key: string]: string } = {
+    grass: 'bg-green-500',
+    poison: 'bg-purple-500',
+    fire: 'bg-red-500',
+    water: 'bg-blue-500',
+    electric: 'bg-yellow-500',
+    flying: "bg-sky-300"
+  };
+  
 
-export default function UserPage({ params }: { params: { username: string } }) {
-    const [difficulty, setDifficulty] = useState<Difficulty>(null);
-    const router = useRouter();
 
-    function getRandomNumber(min: number, max: number) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      }
-      
-      const randomNumber = getRandomNumber(1, 100);
+export default async function UserPage({ params }: { params: { username: string } }) {
+    const starters = [1, 4, 7, 152, 155, 158, 252, 255, 258, 387, 390, 393, 495, 498, 501];
+    const pokemons = await prisma.allPokemon.findMany({
+        where: {
+          id: {
+            in: starters,
+          },
+        },
+      });
 
-    return (
-        <>
-      <div className="min-h-screen flex flex-col items-center bg-gradient-to-t from-gray-900 to-slate-800 justify-center">
-        <h1 className="font-bold text-4xl mb-20">Game configuration</h1>
-        <ChooseDifficulty onSelectDifficulty={setDifficulty} />
-        <button
-          disabled={!difficulty}
-          className={`inline-block text-xl px-6 py-3 rounded-full transition duration-300 ease-in-out transform hover:scale-105 ${difficulty ? 'bg-blue-500 hover:bg-blue-600 text-white' : 'bg-blue-300 text-gray-500 cursor-not-allowed'}`}
-          onClick={() => router.push(`/user/${params.username}/${randomNumber}`)}
-        >
-          {difficulty ? 'Play Game' : 'Select Difficulty'}
-        </button>
-      </div>
-       
-        </>
-    )
-    }
-    
+      async function fetchPokemonTypes() {
+        const pokemonsWithTypes = await Promise.all(pokemons.map(async (pokemon) => {
+          const response = await fetch(pokemon.url);
+          const data = await response.json();
+          return { ...pokemon, types: data.types.map((typeEntry: { type: { name: any; }; }) => typeEntry.type.name) } }))
+        return pokemonsWithTypes;
+      };
+
+    const allData = await fetchPokemonTypes();
+    console.log(allData)
+    const getBgClassForType = (typeName: string) => {
+        return typeToColor[typeName.toLowerCase()] || 'bg-gray-200'
+      };
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-gradient-to-t from-gray-900 to-slate-800 justify-center">
+            {allData.map((pokemon, index) => (
+                <div key={index} className="transform transition duration-500 hover:scale-105 hover:shadow-2xl bg-slate-700 bg-opacity-90 rounded-xl overflow-hidden w-60">
+                  <div className={` ${getBgClassForType(pokemon.types[0])} bg-opacity-20 p-4 flex justify-center items-center`}>
+                    
+                    <img src={pokemon.frontDefault} alt={pokemon.name} className="h-32 w-32 object-contain"/>
+                  </div>
+                  <div className="p-2 flex justify-evenly overflow-hidden">
+                    <h2 className=" text-xl text-slate-200 capitalize">{pokemon.name}</h2>
+                    <div className="flex justify-evenly px-2 ">
+                      {pokemon.types.map((typeName: string) => (
+                        <div key={typeName} className={`${getBgClassForType(typeName)} capitalize px-3 mr-2 py-1 rounded-2xl  text-sm  my-1`}>
+                       
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ))}
+      <h1 className="font-bold text-4xl mb-20">Game configuration</h1>
+
+      <ChooseDifficulty username={params.username} />
+    </div>
+  );
+}
