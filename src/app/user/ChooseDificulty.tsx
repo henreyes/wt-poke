90e7f7871eab420e0745043176138f44a810a876
Henry Reyes <reyesh2@uci.edu>
2023-11-08 23:21:15 -0800

    inclusive settings

--- src/app/user/ChooseDificulty.tsx
+++ src/app/user/ChooseDificulty.tsx
@@ -1,17 +1,16 @@
 // components/ChooseDifficulty.tsx
-"use client"
-// components/ChooseDifficulty.tsx
-"use client"
+"use client";
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
+
 type Difficulty = 'Gen 1' | 'Gen 2' | 'Gen 3' | 'Gen 4' | 'Gen 5';
 
 const typeToColor: { [key in Difficulty]: string } = {
-  "Gen 1": 'bg-green-500 hover:bg-green-600',
-  "Gen 2": 'bg-purple-500 hover:bg-purple-600',
-  "Gen 3": 'bg-red-500 hover:bg-red-600',
-  "Gen 4": 'bg-blue-500 hover:bg-blue-600',
-  "Gen 5": 'bg-orange-500 hover:bg-orange-600',
+  "Gen 1": 'bg-green-500',
+  "Gen 2": 'bg-purple-500',
+  "Gen 3": 'bg-red-500',
+  "Gen 4": 'bg-blue-500',
+  "Gen 5": 'bg-orange-500',
 };
 
 interface ChooseDifficultyProps {
@@ -19,17 +18,9 @@ interface ChooseDifficultyProps {
   starterData: any[]; // Replace 'any' with the appropriate type
 }
 
-const PoketypeToColor: { [key: string]: string } = {
-  grass: 'bg-green-500',
-  poison: 'bg-purple-500',
-  fire: 'bg-red-500',
-  water: 'bg-blue-500',
-  electric: 'bg-yellow-500',
-  flying: "bg-sky-300"
-};
-
 export default function ChooseDifficulty({ username, starterData }: ChooseDifficultyProps) {
   const [selectedDifficulty, setSelectedDifficulty] = useState<Difficulty | null>(null);
+  const [inclusive, setInclusive] = useState<boolean>(false);
   const router = useRouter();
 
   const playGame = () => {
@@ -39,41 +30,53 @@ export default function ChooseDifficulty({ username, starterData }: ChooseDiffic
     }
   };
 
-  const getBgClassForType = (typeName: string) => {
-    return PoketypeToColor[typeName.toLowerCase()] || 'bg-gray-200';
+  const handleSelectDifficulty = (gen: Difficulty) => {
+    setSelectedDifficulty(gen);
+    // If we are selecting a new difficulty, reset the inclusive setting
+    setInclusive(false);
+  };
+
+  const toggleInclusive = () => {
+    setInclusive(!inclusive);
+  };
+
+  const isGenSelected = (gen: Difficulty): boolean => {
+    if (!selectedDifficulty) return false;
+    const selectedGenIndex = ['Gen 1', 'Gen 2', 'Gen 3', 'Gen 4', 'Gen 5'].indexOf(selectedDifficulty);
+    const genIndex = ['Gen 1', 'Gen 2', 'Gen 3', 'Gen 4', 'Gen 5'].indexOf(gen);
+    return inclusive ? genIndex <= selectedGenIndex : genIndex === selectedGenIndex;
   };
 
   return (
-    <div className="flex flex-col items-center space-y-4">
-      <div className="flex space-x-4">
-        {(['Gen 1', 'Gen 2', 'Gen 3', 'Gen 4', 'Gen 5'] as Difficulty[]).map((gen,index) => (
-          <div key={gen} className="group relative">
-            <button
-              className={`w-24 h-24 ${typeToColor[gen]} rounded-full cursor-pointer flex items-center justify-center transform transition duration-300 ease-out ${selectedDifficulty === gen ? 'scale-125' : 'scale-100'}`}
-              onClick={() => setSelectedDifficulty(gen)}
-            >
-              <span className="text-white text-lg font-bold">{gen}</span>
-            </button>
-            <div className={`absolute bottom-0  translate-y-full mb-2 w-96 flex flex-row bg-white p-3 shadow-md rounded-md transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-100`}>
-            {starterData.slice(3 * (index),3 * (index + 1)).map((pokemon, index) => (
-        <div key={index} className="transform transition duration-500 hover:scale-105 hover:shadow-2xl bg-slate-700 bg-opacity-90 rounded-xl overflow-hidden w-60">
-          <div className={` ${getBgClassForType(pokemon.types[0])} bg-opacity-20  flex justify-center items-center`}>
-            <img src={pokemon.frontDefault} alt={pokemon.name} className="h-32 w-32 object-contain"/>
-          </div>
-        </div>
-      ))}
-            </div>
-          </div>
+    <div className="flex flex-col items-center space-y-4 my-8">
+      <div className="grid grid-cols-5 gap-6">
+        {(['Gen 1', 'Gen 2', 'Gen 3', 'Gen 4', 'Gen 5'] as Difficulty[]).map((gen) => (
+          <button
+            key={gen}
+            className={`w-24 h-24 ${typeToColor[gen]} rounded-full cursor-pointer flex items-center justify-center transform transition duration-300 ease-out shadow-lg ${isGenSelected(gen) ? 'ring-4 ring-blue-300' : 'opacity-50'}`}
+            onClick={() => handleSelectDifficulty(gen)}
+          >
+            <span className="text-white text-lg font-bold">{gen}</span>
+          </button>
         ))}
       </div>
+      {selectedDifficulty && (
+        <div className="flex items-center space-x-2 mt-4">
+          <label htmlFor="inclusive-toggle" className="text-white cursor-pointer">
+            Include all generations up to {selectedDifficulty}
+          </label>
+          <div className={`w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer ${inclusive ? 'bg-blue-500' : ''}`} onClick={toggleInclusive}>
+            <div className={`bg-white w-4 h-4 rounded-full shadow-md transform transition duration-300 ease-in-out ${inclusive ? 'translate-x-6' : ''}`}></div>
+          </div>
+        </div>
+      )}
       <button
         disabled={!selectedDifficulty}
-        className={`text-xl px-6 py-3 rounded-full transition duration-300 ease-in-out ${selectedDifficulty ? 'bg-blue-500 hover:bg-blue-600 text-white' : 'bg-blue-300 text-gray-500 cursor-not-allowed'}`}
+        className={`mt-10 text-xl px-6 py-3 rounded-full transition duration-300 ease-in-out transform ${selectedDifficulty ? 'bg-blue-500 hover:bg-blue-700 text-white' : 'bg-blue-300 text-gray-500 cursor-not-allowed'} shadow-md hover:shadow-lg`}
         onClick={playGame}
       >
         {selectedDifficulty ? 'Play Game' : 'Select Difficulty'}
       </button>
-
     </div>
   );
 };
