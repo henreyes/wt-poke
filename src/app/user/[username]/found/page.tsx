7e34d4f1017c68bf3c9dd3d75ccd6d7e2c9ca200
Henry Reyes <reyesh2@uci.edu>
2023-11-09 18:45:32 -0800

    restructure found page

--- src/app/user/[username]/found/page.tsx
+++ src/app/user/[username]/found/page.tsx
@@ -1,7 +1,7 @@
 
 import { prisma } from "@/lib/prisma";
 import { AllPokemon } from "@prisma/client";
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode, Key } from "react";
+import Link from "next/link";
 
 
 const typeToColor: { [key: string]: string } = {
@@ -45,28 +45,35 @@ export default async function Page({ params }: { params: { username: string} }) 
       console.log(res);
       return (
         <>
-          <div className='min-h-screen flex flex-col items-center bg-gradient-to-t from-gray-900 to-indigo-300 py-8'>
-            <h1 className="text-white text-3xl font-bold py-6">{params.username}, here are the Pokémon you found</h1>
-            <div className="flex flex-row flex-wrap w-3/4 items-center justify-center gap-4">
-              {res.map((pokemon, index) => (
-                <div key={index} className="transform transition duration-500 hover:scale-105 hover:shadow-2xl bg-slate-700 bg-opacity-90 rounded-xl overflow-hidden w-60">
-                  <div className={` ${getBgClassForType(pokemon.types[0])} bg-opacity-20 p-4 flex justify-center items-center`}>
-                    
-                    <img src={pokemon.frontDefault} alt={pokemon.name} className="h-32 w-32 object-contain"/>
-                  </div>
-                  <div className="p-2 flex justify-evenly overflow-hidden">
-                    <h2 className=" text-xl text-slate-200 capitalize">{pokemon.name}</h2>
-                    <div className="flex justify-evenly px-2 ">
-                      {pokemon.types.map((typeName: string) => (
-                        <div key={typeName} className={`${getBgClassForType(typeName)} capitalize px-3 mr-2 py-1 rounded-2xl  text-sm  my-1`}>
-                       
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              ))}
+          <div className='min-h-screen flex flex-col items-center bg-gradient-to-t from-gray-900 to-slate-800 '>
+            <h1 className="text-white self-start ml-28 text-3xl py-24">{params.username}, here are the Pokémon you found</h1>
+            <div className="flex flex-row flex-wrap w-full items-center justify-center gap-4">
+            {res.map((pokemon, index) => (
+  <div key={index} className="transform transition duration-500 hover:scale-105 hover:shadow-2xl bg-slate-700 bg-opacity-90 rounded-xl overflow-hidden w-60 flex">
+    <img src={pokemon.frontDefault} alt={pokemon.name} className="w-24 h-24 object-contain"/>
+    <div className="p-2 flex flex-col justify-center items-center">
+      <h2 className="text-xl  text-slate-200 capitalize font-light mb-2">{pokemon.name}</h2>
+      <div className="flex flex-row flex-nowrap overflow-hidden">
+        {pokemon.types.map((typeName: string) => (
+          <div key={typeName} className={`${getBgClassForType(typeName)} capitalize px-2 py-1 rounded-2xl text-sm my-1 mr-1`}>
+            {typeName}
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+))}
             </div>
+
+          <div className="flex flex-row w-2/5 justify-between items-center p-24">
+            <div>
+            <Link href={"/"} className="p-4 w-1/2 bg-gradient-to-b from-indigo-300 to-indigo-400 rounded-2xl text-center">Play again</Link>
+            </div>
+            <div>
+              <Link href={"/"} className="p-4 w-1/2 bg-gradient-to-b from-indigo-700 to-indigo-800 rounded-2xl text-center">View Leaderboard</Link>
+
+            </div>
+          </div>
           </div>
         </>
       )
